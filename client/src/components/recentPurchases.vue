e71d998ecbfd5b8e4f4849a9e40a23460bc074e8
coolcom200 <leonfattakhov@yahoo.com>
2019-09-14 12:56:57 -0400

    Started working on frontend
    - Added filler code
    - Added routing
    - Add navigation drawer

---
+++ client/src/components/recentPurchases.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-list-item two-line>
+    <v-list-item-content>
+      <v-list-item-title>Single-line item</v-list-item-title>
+      <v-list-item-subtitle>Single-line item</v-list-item-subtitle>
+    </v-list-item-content>
+    <v-list-item-icon>
+      <v-icon>mdi-watch</v-icon>
+    </v-list-item-icon>
+  </v-list-item>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,13 +1,82 @@
 <template>
-  <HelloWorld />
+  <div>
+    <v-container>
+      <v-container>
+        <v-layout row>
+          <v-card
+            dark
+            class="col-4"
+          >
+            <v-card-text>
+              <div>Word of the Day</div>
+              <p class="display-1 text--primary">
+                be•nev•o•lent
+              </p>
+              <p>adjective</p>
+              <div class="text--primary">
+                well meaning and kindly.<br>
+                "a benevolent smile"
+              </div>
+            </v-card-text>
+          </v-card>
+          <v-card
+            dark
+            class="mx-auto col-4"
+          >
+            <v-card-text>
+              <div>Word of the Day</div>
+              <p class="display-1 text--primary">
+                be•nev•o•lent
+              </p>
+              <p>adjective</p>
+              <div class="text--primary">
+                well meaning and kindly.<br>
+                "a benevolent smile"
+              </div>
+            </v-card-text>
+          </v-card>
+          <v-card
+            dark
+            class="mx-auto col-4"
+          >
+            <v-card-text>
+              <div>Word of the Day</div>
+              <p class="display-1 text--primary">
+                be•nev•o•lent
+              </p>
+              <p>adjective</p>
+              <div class="text--primary">
+                well meaning and kindly.<br>
+                "a benevolent smile"
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-layout>
+      </v-container>
+
+    </v-container>
+    <v-container>
+      <v-card
+        dark
+        class="mx-auto"
+        tile
+      >
+        <v-card-title>Commonly Purchased Items</v-card-title>
+        <v-list dark>
+          <recentPurchases />
+          <recentPurchases />
+          <recentPurchases />
+          <recentPurchases />
+        </v-list>
+      </v-card>
+    </v-container>
+  </div>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld';
+import recentPurchases from '../components/recentPurchases'
 
 export default {
-  components: {
-    HelloWorld,
-  },
+  components: { recentPurchases },
 };
 </script>
