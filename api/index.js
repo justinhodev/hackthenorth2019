a83f0fdae4fbcd5ff23889909639fd80592ebac3
coolcom200 <leonfattakhov@yahoo.com>
2019-09-14 14:54:15 -0400

    Updated server to interface with firebase firestore

--- api/index.js
+++ api/index.js
@@ -1,20 +1,6 @@
 const EXPRESS = require("express");
 const APP = EXPRESS();
-const FIREBASE_ADMIN = require("firebase-admin");
-const FIREBASE_CONFIG = require("./config.json");
 const API_PORT = 8081;
-const CONFIG_ERROR = "databaseName and the service key must be set";
-const DATABASE_NAME = FIREBASE_CONFIG["firebaseDatabaseName"];
-const SERVICE_KEY_PATH = FIREBASE_CONFIG["serviceKeyPath"];
-
-if (!DATABASE_NAME || !SERVICE_KEY_PATH) {
-  console.error(CONFIG_ERROR);
-}
-
-FIREBASE_ADMIN.initializeApp({
-  credential: FIREBASE_ADMIN.credential.cert(SERVICE_KEY_PATH),
-  databaseURL: `https://${DATABASE_NAME}.firebaseio.com`
-});
 
 APP.get("/", function(req, res) {
   res.send("hello world");
