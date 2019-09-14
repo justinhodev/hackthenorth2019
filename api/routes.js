0a935864dafa1144b896677d49cc5ccbae912a00
Justin Ho <justinhodev@gmail.com>
2019-09-14 14:21:41 -0700

    Add Comments

--- api/routes.js
+++ api/routes.js
@@ -3,10 +3,13 @@ const receipts = require('./receipt');
 
 let router = express.Router();
 
+// Get all receipts
 router.get('/receipts', receipts.getReceipts);
 
+// Get single receipt
 router.get('/receipts/:store', receipts.getReceiptByStore);
 
+// Create a receipt
 router.post('/receipts', receipts.postReceipt);
 
 module.exports = router;
