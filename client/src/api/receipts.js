ac1e314a40b7231054bc5770b1ffde7831d0c581
Justin Ho <justinhodev@gmail.com>
2019-09-15 01:31:00 -0700

    Remove Exposing URL From Console

--- client/src/api/receipts.js
+++ client/src/api/receipts.js
@@ -3,9 +3,7 @@ import axios from 'axios';
 const apiUrl = process.env.VUE_APP_API_URL + "/receipts";
 
 export async function getAllReceipts() {
-  console.log(apiUrl);
   let response = await axios.get(apiUrl);
   //localStorage.setItem("receipts", response);
-  console.log(response);
   return response;
 }
