54775cc8a90a0ca8867476144c74cf647f54ce9e
GleeBright <55309490+GleeBright@users.noreply.github.com>
2019-09-14 21:24:30 -0400

    Circle and Bar graph components added

---
+++ client/src/components/Circleplot.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+        <apexchart
+          height="250"
+          type="donut"
+          :options="chartOptions"
+          :series="series"
+        ></apexchart>
+    </div>
+</template>
+
+<script>
+import VueApexCharts from "vue-apexcharts";
+import Vue from "vue";
+
+Vue.use(VueApexCharts);
+export default {
+data: () => {
+    return {
+      chartOptions: {},
+      series: [44, 55, 41, 17, 15]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
