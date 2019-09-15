c33ae407c7062f898b96bb1a65b60dc8040266cb
Justin Ho <justinhodev@gmail.com>
2019-09-14 19:46:38 -0700

    Add Vuex for Receipts

--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,25 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import { getAllReceipts } from './api/receipts';
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    receipts: [],
+    items: []
   },
   mutations: {
-
+    setReceipts(state, products) {
+      state.receipts = products;
+    }
   },
   actions: {
-
+    getReceipts({ commit }) {
+      getAllReceipts().then(receipts => {
+        commit('setReceipts', receipts);
+      });
+    }
   }
 })
